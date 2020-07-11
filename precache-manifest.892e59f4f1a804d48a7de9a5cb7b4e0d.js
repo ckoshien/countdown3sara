180b65baade82a0a84d949e748821f95134e2265
ckoshien <ckoshien@gmail.com>
2020-07-11 11:11:09 +0900

    Updates

---
+++ precache-manifest.892e59f4f1a804d48a7de9a5cb7b4e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759cf47700b4f0357986bfd8a610e988",
+    "url": "/countdown3sara/index.html"
+  },
+  {
+    "revision": "49271683507001f039b6",
+    "url": "/countdown3sara/static/css/main.303d511d.chunk.css"
+  },
+  {
+    "revision": "3c7a196f2846ae0d6896",
+    "url": "/countdown3sara/static/js/2.915f6b7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown3sara/static/js/2.915f6b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49271683507001f039b6",
+    "url": "/countdown3sara/static/js/main.844dd649.chunk.js"
+  },
+  {
+    "revision": "69d4eae13dba1ccf2f7e",
+    "url": "/countdown3sara/static/js/runtime-main.78351555.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b588f96cd4716d0fbe327017cb13ebd.js"
+  "/countdown3sara/precache-manifest.892e59f4f1a804d48a7de9a5cb7b4e0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/countdown3sara/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
