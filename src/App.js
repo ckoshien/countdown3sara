e83fc601aeffe1d728e047acbcfdd95c74666a4e
ckoshien <ckoshien@gmail.com>
2020-07-11 11:06:07 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import moment from 'moment';
 
 function App() {
+  const [displayT, setDisplayT] = useState();
+  const duration = moment.duration( moment('2020-08-15 09:00').diff(moment()) );;
+  setInterval(
+    ()=>{
+      setDisplayT(duration);
+    },1000
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        margin:5,
+        position:'relative'
+      }}
+    >
+      <img
+        src={'/images/promo.jpg'}
+        style={{
+          width:'95vw',
+          maxWidth:800
+        }}
+        alt={'画像'}
+      />
+      <div
+        style={{
+          position:'absolute',
+          color:'red',
+          fontSize:'5.2vw',
+          fontWeight:'bold',
+          bottom: '12vw',
+          left:'7.5vw',
+          transform:'rotate(-15deg)'
+        }}
+      >
+        あと{Math.floor(duration.asDays())}日
+        <div
+          style={{
+            fontSize:'3vw'
+          }}
         >
-          Learn React
-        </a>
-      </header>
+        {duration.hours()}時間
+        {duration.minutes()}分
+        {duration.seconds()}秒
+        </div>
+      </div>
+      
     </div>
   );
 }
