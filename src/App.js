4407b06f29b3d7d5478605d401f03b2c00509b3d
ckoshien <ckoshien@gmail.com>
2020-07-15 08:27:37 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import moment from "moment";
 
 function App() {
   const [displayT, setDisplayT] = useState();
-  const duration = moment.duration( moment('2020-08-15 09:00').diff(moment()) );;
-  useEffect(()=>{
-    const interval = setInterval(
-      ()=>{
-        setDisplayT(duration);
-      },1000
-    )
-    return()=>{
+  const duration = moment.duration(moment("2020-08-15 09:00").diff(moment()));
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDisplayT(duration);
+    }, 1000);
+    return () => {
       clearInterval(interval);
-    }
-  },[duration])
-
+    };
+  }, [duration]);
 
   return (
     <>
-    <div
-      style={{
-        margin:5,
-        position:'relative'
-      }}
-    >
-      <img
-        src={'./images/promo.jpg'}
-        style={{
-          width:'95vw',
-          //maxWidth:800
-        }}
-        alt={'画像'}
-      />
       <div
         style={{
-          position:'absolute',
-          color:'red',
-          fontSize:'5.2vw',
-          fontWeight:'bold',
-          bottom: '12vw',
-          left:'7.5vw',
-          transform:'rotate(-15deg)'
+          margin: 5,
+          position: "relative",
         }}
       >
-        あと{Math.floor(duration.asDays())}日
+        <img
+          src={"./images/promo.jpg"}
+          style={{
+            width: "95vw",
+            //maxWidth:800
+          }}
+          alt={"画像"}
+        />
         <div
           style={{
-            fontSize:'3vw'
+            position: "absolute",
+            color: "red",
+            fontSize: "5.2vw",
+            fontWeight: "bold",
+            bottom: "12vw",
+            left: "7.5vw",
+            transform: "rotate(-15deg)",
           }}
         >
-        {zeroPadding(duration.hours(),2)}時間
-        {zeroPadding(duration.minutes(),2)}分
-        {zeroPadding(duration.seconds(),2)}秒
+          あと{Math.floor(duration.asDays())}日
+          <div
+            style={{
+              fontSize: "3vw",
+            }}
+          >
+            {zeroPadding(duration.hours(), 2)}時間
+            {zeroPadding(duration.minutes(), 2)}分
+            {zeroPadding(duration.seconds(), 2)}秒
+          </div>
         </div>
       </div>
-    </div>
-    <div>
+      <div>
         <img
           style={{
-            width:'95vw'
-          }} 
-          src={'https://res.cloudinary.com/dgonrnjmq/image/upload/v1593469234/cap-baseball/649/NLAA9WRrS4mHjlnZCUUb_ow54ij.png'}/>
+            width: "95vw",
+          }}
+          src={
+            "https://res.cloudinary.com/dgonrnjmq/image/upload/v1593469234/cap-baseball/649/NLAA9WRrS4mHjlnZCUUb_ow54ij.png"
+          }
+        />
       </div>
     </>
   );
@@ -69,6 +69,6 @@ function App() {
 
 export default App;
 
-function zeroPadding(NUM, LEN){
-	return ( Array(LEN).join('0') + NUM ).slice( -LEN );
+function zeroPadding(NUM, LEN) {
+  return (Array(LEN).join("0") + NUM).slice(-LEN);
 }
